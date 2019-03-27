326bac7fc241d00ddc4fe442406583b2daa6eb87
Hông Duc <hong-duc.ta@protonmail.com>
2019-03-27 17:07:21 +0100

    feat: added OAuth for Google

--- src/Authentification/Authentification.js
+++ src/Authentification/Authentification.js
@@ -1,12 +1,41 @@
 import React, { Component } from 'react';
 import './Authentification.css';
 
-class Authentification extends Component
-{
-    render()
+import "tabler-react/dist/Tabler.css";
+
+import { Grid, LoginPage } from "tabler-react";
+
+class Authentification extends Component {
+
+    onSignIn(googleUser) {
+        var profile = googleUser.getBasicProfile();
+        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+        console.log('Name: ' + profile.getName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    }
+
+    Login()
     {
-        return(
-            <div></div>
+        console.log("TEST");
+    }
+
+    render() {
+        return (
+            <div>
+                <Grid>
+                    <Grid.Row>
+                        <Grid.Col>Bienvenue !</Grid.Col>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Col offset="5">
+                            <div className="g-signin2" data-onsuccess="onSignIn"></div>
+                        </Grid.Col>
+                    </Grid.Row>
+                </Grid>
+                <LoginPage method="Login()"/>
+            </div>
+
         );
     }
 }
--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import Authentification from './Authentification/Authentification';
 
 //ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<Authentification />, document.getElementById('root'));
-ReactDOM.render(<Dashboard />, document.getElementById('root'));
+//ReactDOM.render(<Dashboard />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
